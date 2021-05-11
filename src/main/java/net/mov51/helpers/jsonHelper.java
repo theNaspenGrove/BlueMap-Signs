5853620d7d4d6524dd3651bedfb8e4b9d4c673ac
mov51 <mov51@mov51.net>
2021-05-10 22:18:16 -0400

    No need to use JSON storage I'm building the marker ID with the exact sign location, so I can easily generate the ID again when a sign is broken and attempt to remove any markers with that ID.

--- src/main/java/net/mov51/helpers/jsonHelper.java
+++
@@ -1,7 +0,0 @@
-package net.mov51.helpers;
-
-public class jsonHelper {
-    //TODO store current sign information
-    // Needs to store based on sign type
-    // Store just the marker name, marker ID, sign location, and chosen icon
-}
